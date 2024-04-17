1c11543a9be6bb5efbd94b688d593547dac1834c
RyanFoerster <ryanfoerster@outlook.be>
2024-04-17 12:58:37 +0200

    Fix some import

--- src/app/footer/accordion-footer/accordion-footer.component.ts
+++ src/app/footer/accordion-footer/accordion-footer.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { BrnAccordionContentComponent } from '@spartan-ng/ui-accordion-brain';
 import {
   HlmAccordionContentDirective,
   HlmAccordionDirective,
@@ -21,7 +20,6 @@ import { HlmIconComponent } from '../../../../components/ui-icon-helm/src/lib/hl
     HlmAccordionIconDirective,
     HlmAccordionItemDirective,
     HlmAccordionTriggerDirective,
-    BrnAccordionContentComponent,
     HlmIconComponent,
   ],
 })
